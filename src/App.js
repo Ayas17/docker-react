8b956efef4e4764b281a5abd895b66be1d2b77f9
ayandamkula <ayanda@opencartplugins.net>
2021-07-23 14:38:42 +0200

    Initial version of the project commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>        <p>
+          Hello there my name is Ayanda Mkula.
         </p>
         <a
           className="App-link"
